43f67c692a89c5d4a2f6bea494c35789adb0f405
Jonathan Lewis <jonathan.lewis@welch.edu>
2023-06-20 15:50:34 -0500

    It reads from a file and prints!

--- Veggie_Tales_Form.cpp
+++ Veggie_Tales_Form.cpp
@@ -6,7 +6,7 @@
 #include <string>
 using namespace std;
 
-const string records_path = "users/jonathanlewis/documents/Xcode/Veggie_Tales_Registration_Form2/records.txt";
+const string records_path = "/Users/jonathanlewis/Documents/Xcode/Veggie_Tales_Registration_Form2/records.txt";
 
 //vector<string> read_line_from_records_file(ifstream file_object);
 ifstream file_stream_from_records(string file_path);
@@ -17,6 +17,13 @@ int main(){
     vector<vector<string>> form_responses{};
     ifstream file_object = file_stream_from_records(records_path);
     read_line_from_records_file(file_object, form_responses);
+    
+    for(int i = 0; i < form_responses.size(); i++){
+        for(int c=0; c< form_responses[i].size(); c++){
+            cout << form_responses[i][c];
+        }
+    }
+
     cout << "end of the program" << endl;
     return 0;
 }
