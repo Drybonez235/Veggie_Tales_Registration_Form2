9d484b13e57ee428ccba659a657565a2bb13dae7
Jonathan Lewis <jonathan.lewis@welch.edu>
2023-06-27 10:42:04 -0500

    Alright, So now we have a generic content class and a file reader class. The file reader just reads from a file and places the values into a vector of vectors. The content class will take a vector of values, a vector of value length limits, and a vector of value names, and will make sure that everything is good with the lengths.

--- Veggie_Tales_Form.cpp
+++ Veggie_Tales_Form.cpp
@@ -5,51 +5,20 @@
 #include <sstream>
 #include <string>
 #include "content.h"
+#include "file_reader.h"
 
 using namespace std;
 
-const string records_path = "/Users/jonathanlewis/Documents/Xcode/Veggie_Tales_Registration_Form2/records.txt";
+const string file_path = "/Users/jonathanlewis/Documents/Xcode/Veggie_Tales_Registration_Form2/records.txt";
 
 //vector<string> read_line_from_records_file(ifstream file_object);
-ifstream file_stream_from_records(string file_path);
-void read_line_from_records_file(ifstream &file_object, vector<vector<string>>& form_responses);
-
 int main(){
+    file_reader file_1(file_path);
     
-    vector<vector<string>> form_responses{};
-    ifstream file_object = file_stream_from_records(records_path);
-    read_line_from_records_file(file_object, form_responses);
-    
-    for(int i = 0; i < form_responses.size(); i++){
-        for(int c=0; c < form_responses[i].size(); c++){
-            cout << form_responses[i][c] << endl;
-        }
-    }
-
+        
     cout << "end of the program" << endl;
     return 0;
 }
 
-ifstream file_stream_from_records(string file_path){
-    ifstream input_reader(file_path);
-    return  input_reader;
-}
-
-void read_line_from_records_file(ifstream &file_object, vector<vector<string>>& form_responses){
-    string line, record, temp;
-    vector<string> row;
-    
-    while(file_object){
-            row.clear();
-            getline(file_object, line);
-            
-            stringstream s(line);
-            while(getline(s, record, ',')) {
-                row.push_back(record);
-            }
-            //Sends the records to the function
-            form_responses.push_back(row);
-    }
-}
 
 
--- content.h
+++ content.h
@@ -7,29 +7,31 @@ using namespace std;
 class generic_form{
     
 public:
-    static const int form_fields = 26;
-    static constexpr int field_lengths[form_fields] = {43, 13, 7, 6, 49, 17, 5, 15, 24, 23, 24, 90, 10, 25, 25, 36, 33, 36, 33, 5, 96, 92, 93, 84, 8, 48};
     map<string, string> child_map;
-    const string values_array[form_fields];
-    //= {"name", "dob", "age", "sex", "address", "city", "state", "zip", "home_phone", "cell_phone", "other_phone", "email", "email_list", "parent_1", "parent_2", "emergency_contact1_name", "emeergency_contact1_phone", "emergency_contact2_name", "emergency_contact2_phone", "medical_issues", "allergies", "medications", "diet", "special", "walk_home", "aproved_pickup_persons"};
+    
+    generic_form(){
+    
+    }
     
     void set_map_value(string key, string value){
         child_map.insert(make_pair(key, value));
     }
     
-    void check_value_length(vector<string> &record_values){
+    void check_value_length_and_add_to_map(vector<string> &record_values, vector<int> &field_lengths, vector<string> &record_values_names){
         int record_value_count = record_values.size();
         
-        if(record_value_count > form_fields){
+        if(record_value_count > record_values_names.size()){
             cout << "There were more records then fields!!!";
         }
-        int value_character_count;
         
+        int value_character_count;
+        int value_character_limits;
         
         for(int i = 0; i < record_value_count; i++){
         value_character_count = record_values[i].size();
-            if(value_character_count < field_lengths[i]){
-                set_map_value(values_array[i], record_values[i]);
+        value_character_limits = field_lengths[i];
+            if(value_character_count < value_character_limits){
+                set_map_value(record_values_names[i], record_values[i]);
                 
             } else {
                     cout << "Value Skipped" << endl;
